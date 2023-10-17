efa68564b57aa9dd6684ce94a5c2737d57ce492e
aminpirakand <amin.pirakand@yahoo.com>
2023-10-17 19:40:37 +0330

    install react router and & Shop page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import {Routes, Route} from 'react-router-dom'
 import { Container } from "react-bootstrap"
 import Navbar from "./components/Navbar"
+import Shop from './pages/Shop'
 function App() {
  return (
   <Container>
     <Navbar/>
+    <Routes>
+      <Route index element={<Shop/>}/>
+    </Routes>
   </Container>
  )
 }
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,5 @@
+
+function Shop(){
+  return <h2>Shoping Cart</h2>
+}
+export default Shop
