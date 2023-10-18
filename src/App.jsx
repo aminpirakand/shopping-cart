09efff2bcedb2ae1e6c8e90838812a806017b608
aminpirakand <amin.pirakand@yahoo.com>
2023-10-18 09:27:19 +0330

    Added About page for test router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import {Routes, Route} from 'react-router-dom'
 import { Container } from "react-bootstrap"
+
 import Navbar from "./components/Navbar"
 import Shop from './pages/Shop'
+import About from "./pages/About"
+
 function App() {
  return (
   <Container>
     <Navbar/>
     <Routes>
       <Route index element={<Shop/>}/>
+      <Route path="/about" element={<About/>}/>
     </Routes>
   </Container>
  )
